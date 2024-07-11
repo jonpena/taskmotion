0df7a07a65d9519cd07e5ebe16969be8cf891e46
jonpena <gran_200014@hotmail.com>
2024-07-11 04:25:42 +0200

    fix: :bug: Se soluciono problema de double click en el componente Checkbox

--- client/src/components/UI/Checkbox.tsx
+++ client/src/components/UI/Checkbox.tsx
@@ -2,7 +2,8 @@ import { CheckIcon } from 'lucide-react';
 
 type Props = React.InputHTMLAttributes<HTMLInputElement>;
 
-function Checkbox({ name, checked, onChange, disabled, className }: Props) {
+// million-ignore
+const Checkbox = ({ name, checked, onChange, disabled, className }: Props) => {
   return (
     <label className='flex h-fit w-fit'>
       <input
@@ -11,15 +12,13 @@ function Checkbox({ name, checked, onChange, disabled, className }: Props) {
         checked={checked}
         onChange={onChange}
         type='checkbox'
-        className={
-          `peer pointer-events-none w-0 appearance-none opacity-0` + className
-        }
+        className={`peer pointer-events-none w-0 appearance-none` + className}
       />
       <div className='pointer-events-auto relative aspect-square bg-black/5 transition-colors hover:bg-black/10 peer-checked:bg-text w-5 h-5 rounded-lg peer-checked:[&>*]:animate-pop-up peer-checked:[&>*]:opacity-100'>
-        <CheckIcon className='absolute inset-0 m-auto w-[10px] animate-none stroke-[6px] text-foreground opacity-0 transition-opacity' />
+        <CheckIcon className='absolute inset-0 m-auto w-[10px] animate-none stroke-[6px] text-foreground opacity-0' />
       </div>
     </label>
   );
-}
+};
 
 export default Checkbox;
