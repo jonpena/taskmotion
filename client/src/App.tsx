b7c7e40cd75af52ff0e19ad3106fc4daaa80ac89
jonpena <gran_200014@hotmail.com>
2024-07-27 20:44:31 +0200

    feat: :art: Ahora el componente list se carga como una subruta de Board
    Ahora el componente List se carga como una subruta usando Outlet component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Board from './pages/Board';
+import List from '@/components/List';
 
 export function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/list/:listId' element={<Board />} />
+      <Route path='/list/:listId' element={<Board />}>
+        <Route index element={<List />} />
+      </Route>
     </Routes>
   );
 }
--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -1,7 +1,8 @@
 import CreateTask from '@/components/CreateTask';
-import List from '@/components/List';
+// import List from '@/components/List';
 import ListCollection from '@/components/ListCollection';
 import { UserAuth } from '@/context/AuthContext';
+import { Outlet } from 'react-router-dom';
 
 const Board = () => {
   const { signout } = UserAuth();
@@ -10,7 +11,7 @@ const Board = () => {
     <div>
       <CreateTask />
       <ListCollection />
-      <List />
+      <Outlet />
       <div className='absolute text-white top-2 right-3 z-[999] border border-gray-500 px-3 py-2 rounded-md bg-indigo-500'>
         <button onClick={signout}>Signout</button>
       </div>
