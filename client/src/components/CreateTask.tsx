07f00dcbd140e5069cc4f9d7028f90ef40ed2755
jonpena <gran_200014@hotmail.com>
2024-09-16 00:20:27 +0200

    feat: :art: Se mejoro el componente CreateTask.
    se le agrego un button de crear new task. validaciones y mucho mas

--- client/src/components/CreateTask.tsx
+++ client/src/components/CreateTask.tsx
@@ -6,9 +6,10 @@ import { requestUpdateList } from '@/services/requestUpdateList';
 import { useListStore } from '@/store/listStore';
 import { Input } from '@/components/UI/input';
 import Checkbox from '@/components/UI/checkbox';
+import { Plus } from 'lucide-react';
 
 const CreateTask = () => {
-  const [inputData, setInputData] = useState('');
+  const [taskName, setTaskName] = useState('');
   const { setTasks } = useTaskStore();
   const tasks = useTaskStore((state) => state.tasks);
   const { setLists } = useListStore();
@@ -16,44 +17,59 @@ const CreateTask = () => {
   const { listId } = useParams();
   const [checked, setChecked] = useState(false);
 
-  const handleKeyPress = (e: React.KeyboardEvent) => {
-    if (!listId) return;
+  const createTask = (name: string) => {
+    if (!listId || !name) return;
 
-    if (e.key === 'Enter' && (e.target as HTMLInputElement).value !== '') {
-      setInputData('');
-      const newTask = {
-        id: uuid(),
-        name: (e.target as HTMLInputElement).value,
-        checked,
-      };
-      const newTasksArray = [newTask, ...tasks];
-      requestUpdateList(listId, { tasks: newTasksArray });
-      const updateLists = [...lists];
-      const index = lists.findIndex((l) => l.listId === listId);
-      updateLists[index].tasks = newTasksArray;
-      setTasks(newTasksArray);
-      setLists([...updateLists]);
+    const newTask = {
+      id: uuid(),
+      name,
+      checked,
+    };
+    const newTasksArray = [newTask, ...tasks];
+    requestUpdateList(listId, { tasks: newTasksArray });
+    const updateLists = [...lists];
+    const index = lists.findIndex((l) => l.listId === listId);
+    updateLists[index].tasks = newTasksArray;
+    setTaskName('');
+    setTasks(newTasksArray);
+    setLists([...updateLists]);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      createTask(taskName);
     }
   };
 
+  const handleClick = () => createTask(taskName);
+
   return (
-    <div className='mx-auto flex max-w-80 sticky top-2'>
-      <Input
-        disabled={listId === 'home'}
-        type='text'
-        placeholder='Create new task...'
-        className='sticky z-20 rounded pl-4 outline-none w-full h-12 pr-12 border-none bg-gray-100 hover:bg-gray-200 focus-visible:ring-0 focus-visible:bg-gray-200 focus-visible:placeholder:text-gray-400'
-        onKeyDown={handleKeyPress}
-        onChange={(e) => setInputData(e.target.value)}
-        value={inputData}
-      />
+    <div className='mx-auto flex items-center max-w-80 sticky top-2 bg-gray-100 rounded px-2'>
       <Checkbox
         disabled={listId === 'home'}
         name='checked'
         checked={checked}
         onChange={(e) => setChecked(e.target.checked)}
-        className='top-4 right-8 z-30'
+        className=''
       />
+      <Input
+        disabled={listId === 'home'}
+        type='text'
+        placeholder='Create new task...'
+        className='text-gray-600 px-4 outline-none w-full h-12 border-none focus-visible:ring-0 focus-visible:placeholder:text-gray-400'
+        onKeyDown={handleKeyPress}
+        onChange={(e) => setTaskName(e.target.value.trimStart())}
+        value={taskName}
+      />
+
+      <button
+        title='Create new task'
+        onClick={handleClick}
+        className=' w-8 h-7 right-2 top-3 flex justify-center items-center 
+        text-sm font-medium flex-grow-1 rounded-lg select-none bg-white'
+      >
+        <Plus className='w-4 h-4 text-gray-600 pointer-events-none' />
+      </button>
     </div>
   );
 };
