e65acea1c4c9f6153e60ed2441abd14a86133b74
jonpena <gran_200014@hotmail.com>
2024-09-20 00:52:55 +0200

    refactor: :art: se agrego el componente UserWelcome al componente Board

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -1,12 +1,18 @@
 import CreateTask from '@/components/CreateTask';
 import ListCollection from '@/components/ListCollection';
 import { UserNav } from '@/components/UserNav';
+import UserWelcome from '@/components/UserWelcome';
 import { Outlet } from 'react-router-dom';
 
 const Board = () => {
   return (
     <div>
-      <CreateTask />
+      <main className='px-14 lg:px-0 w-full lg:pl-[340px] sticky top-2 lg:top-40'>
+        <div className='max-w-[640px] min-w-48 lg:w-[640px] mx-auto flex flex-col lg:gap-y-5'>
+          <UserWelcome />
+          <CreateTask />
+        </div>
+      </main>
       <nav className='absolute top-4 right-2 z-[999]'>
         <UserNav />
       </nav>
