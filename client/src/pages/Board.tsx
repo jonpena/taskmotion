80e9a0e10c8c4929f008f296edcdad8380ad5ca5
jonpena <gran_200014@hotmail.com>
2024-09-22 03:04:54 +0200

    chore: :art: estructurando mejor el componente Board.tsx

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -7,15 +7,15 @@ import { Outlet } from 'react-router-dom';
 const Board = () => {
   return (
     <div>
+      <nav className='absolute top-4 right-2 z-[999]'>
+        <UserNav />
+      </nav>
       <main className='px-14 lg:px-0 w-full lg:pl-[340px] sticky top-2 lg:top-40'>
         <div className='max-w-[640px] min-w-48 lg:w-[640px] mx-auto flex flex-col lg:gap-y-5'>
           <UserWelcome />
           <CreateTask />
         </div>
       </main>
-      <nav className='absolute top-4 right-2 z-[999]'>
-        <UserNav />
-      </nav>
       <ListCollection />
       <Outlet />
     </div>
