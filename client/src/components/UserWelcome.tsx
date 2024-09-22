ce81c13e9ee3ce2c1b0ea88271fac4afde3a8b98
jonpena <gran_200014@hotmail.com>
2024-09-22 03:05:53 +0200

    feat: :zap: el componente UserWelcome tiene el nombre de la lista y actualiza el title de la page

--- client/src/components/UserWelcome.tsx
+++ client/src/components/UserWelcome.tsx
@@ -1,16 +1,23 @@
 import { UserAuth } from '@/context/AuthContext';
+import { useAlertDialogStore } from '@/store/alertDialogStore';
 import { getGreeting } from '@/utils/getGreeting';
+import { useDocumentTitle } from '@uidotdev/usehooks';
 import { format } from 'date-fns';
+import { useParams } from 'react-router-dom';
 
 const UserWelcome = () => {
   const { user } = UserAuth();
   const formatedDate = format(new Date(), 'EEEE, MMMM d');
+  const { title } = useAlertDialogStore();
+  const { listId } = useParams();
+
+  useDocumentTitle('Taskmotion | ' + (listId !== 'home' ? title : 'Home'));
 
   return (
     <div className='hidden lg:block'>
       <h3 className='text-gray-400'>{formatedDate}</h3>
       <h1 className='text-gray-700 text-2xl mt-1'>
-        {getGreeting()}, {user.fullname}
+        {listId === 'home' ? `${getGreeting()}, ${user.fullname}` : title}
       </h1>
     </div>
   );
