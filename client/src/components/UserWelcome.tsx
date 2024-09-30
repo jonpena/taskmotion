ec594b99eaee8059a4e22bfd6fc5d11b3aca24b6
jonpena <gran_200014@hotmail.com>
2024-09-30 02:01:24 +0200

    fix: :bug: se le agregaron unos loading mientras espera el title de la lista. tanto para el componente y tag

--- client/src/components/UserWelcome.tsx
+++ client/src/components/UserWelcome.tsx
@@ -1,5 +1,5 @@
 import { UserAuth } from '@/context/AuthContext';
-import { useAlertDialogStore } from '@/store/alertDialogStore';
+import { useAlertDialogStore } from '@/store/dialogStore';
 import { getGreeting } from '@/utils/getGreeting';
 import { useDocumentTitle } from '@uidotdev/usehooks';
 import { format } from 'date-fns';
@@ -11,13 +11,17 @@ const UserWelcome = () => {
   const { title } = useAlertDialogStore();
   const { listId } = useParams();
 
-  useDocumentTitle('Taskmotion | ' + (listId !== 'home' ? title : 'Home'));
+  useDocumentTitle(
+    'Taskmotion | ' + (listId !== 'home' ? title ?? 'Loading...' : 'Home')
+  );
 
   return (
     <div className='hidden lg:block'>
       <h3 className='text-gray-400'>{formatedDate}</h3>
-      <h1 className='text-gray-700 text-2xl mt-1'>
-        {listId === 'home' ? `${getGreeting()}, ${user.fullname}` : title}
+      <h1 className='text-gray-700 text-2xl font-medium mt-1'>
+        {listId === 'home'
+          ? `${getGreeting()} - ${user?.fullname ?? 'Loading...'}`
+          : `${getGreeting()} - ${title ?? 'Loading...'}`}
       </h1>
     </div>
   );
