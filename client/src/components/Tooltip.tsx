b09c6f658b43f6eda6ccaef30439610296f5a47a
jonpena <gran_200014@hotmail.com>
2024-09-30 02:09:43 +0200

    feat: :sparkles: creando componente Tooltips personalizado

---
+++ client/src/components/Tooltip.tsx
@@ -0,0 +1,27 @@
+import {
+  Tooltip as TooltipPrimitive,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+type TooltipProps = {
+  title: string;
+  disable?: boolean;
+  children: React.ReactNode | React.ReactNode[];
+};
+
+export function Tooltip({ title, disable = false, children }: TooltipProps) {
+  if (disable) return <>{children}</>;
+
+  return (
+    <TooltipProvider delayDuration={500}>
+      <TooltipPrimitive defaultOpen={false}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent>
+          <p>{title}</p>
+        </TooltipContent>
+      </TooltipPrimitive>
+    </TooltipProvider>
+  );
+}
