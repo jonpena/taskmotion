795a83adf56cb943bc877610ea2a3fcd130d06b2
jonpena <gran_200014@hotmail.com>
2024-10-25 01:29:42 +0200

    refactor: :fire: se removio array constante de lista que no estaba en uso

--- client/src/constants/base.ts
+++ client/src/constants/base.ts
@@ -1,13 +1,2 @@
-import { ListProps } from '../../../shared/interfaces/list.interface';
-
 export const TASK_MAXWIDTH = 640;
 export const TASK_MINWIDTH = 375;
-
-export const LIST_DEFAULT: ListProps[] = [
-  {
-    listId: '0',
-    name: 'Home',
-    tasks: [],
-    created_at: '',
-  },
-];
