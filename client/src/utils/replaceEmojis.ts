8e4620cf8868879ca829b37de3bb98e6080d2d3e
jonpena <gran_200014@hotmail.com>
2024-10-25 01:34:28 +0200

    fix: :bug: la funcion estaba transformando todo el input en lowercase

--- client/src/utils/replaceEmojis.ts
+++ client/src/utils/replaceEmojis.ts
@@ -2,8 +2,8 @@ import { EMOJIS } from '@/constants/emojis';
 
 export const replaceEmojis = (text: string) => {
   for (const key in EMOJIS) {
-    const regex = new RegExp(`:${key}`, 'g'); // Crea una expresión regular para buscar el token :key:
-    text = text.toLocaleLowerCase().replace(regex, EMOJIS[key]); // Reemplaza todas las ocurrencias
+    const regex = new RegExp(`:${key}`, 'ig'); // Crea una expresión regular para buscar el token :key:
+    text = text.replace(regex, EMOJIS[key]); // Reemplaza todas las ocurrencias
   }
   return text;
 };
