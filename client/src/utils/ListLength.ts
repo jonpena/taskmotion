deda02ca792683736bba3b4d709051626881b64f
jonpena <gran_200014@hotmail.com>
2025-01-09 05:41:54 +0100

    refactor: :fire: removing code and renaming functions and componentes

--- client/src/utils/ListLength.ts
+++
@@ -1,8 +0,0 @@
-import { ListProps } from '@shared/list.interface';
-import { TaskProps } from '@shared/task.interface';
-
-export const listLength = (
-  list: ListProps,
-  tasks: TaskProps[],
-  listId?: string
-) => (list.listId === listId ? tasks.length : list.tasks.length);
