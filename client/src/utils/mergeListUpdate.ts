8e77792d19514370b0f98f86b84edebc32d85a15
jonpena <gran_200014@hotmail.com>
2025-01-26 03:26:58 +0100

    docs: :fire: renaming functions and components

--- client/src/utils/mergeListUpdate.ts
+++
@@ -1,9 +0,0 @@
-import { TaskProps } from '@shared/task.interface';
-import { ListProps } from '@shared/list.interface';
-
-export const mergeListUpdate = (id: string, lists: ListProps[], updatedTasks: TaskProps[]
-): ListProps[] => {
-  return [...lists].map((l: ListProps) =>
-    l.listId === id ? { ...l, tasks: updatedTasks } : l
-  );
-};
