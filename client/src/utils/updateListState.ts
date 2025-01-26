9156d09c52cbed2b6b4511bb58138dc2c64026df
jonpena <gran_200014@hotmail.com>
2025-01-26 03:27:30 +0100

    docs: :art: rename functions

---
+++ client/src/utils/updateListState.ts
@@ -0,0 +1,12 @@
+import { TaskProps } from '@shared/task.interface';
+import { ListProps } from '@shared/list.interface';
+
+export const updateListState = (
+  id: string,
+  lists: ListProps[],
+  updatedTasks: TaskProps[]
+): ListProps[] => {
+  return [...lists].map((l: ListProps) =>
+    l.listId === id ? { ...l, tasks: updatedTasks } : l
+  );
+};
---
+++ client/src/utils/updateTaskState.ts
@@ -0,0 +1,22 @@
+import { TaskProps } from '@shared/task.interface';
+
+export const updateTaskState = (
+  id: string,
+  tasks: TaskProps[],
+  task: Partial<TaskProps>
+): TaskProps[] => {
+  const taskIndex = tasks.findIndex((t) => t.id === id);
+
+  // Validación para asegurarse de que la tarea existe
+  if (taskIndex === -1) {
+    throw new Error(`Task with id ${id} not found`);
+  }
+
+  const updateTasks = [...tasks];
+  updateTasks[taskIndex] = {
+    ...updateTasks[taskIndex],
+    ...task,
+  };
+
+  return updateTasks;
+};
