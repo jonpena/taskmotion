bf2a401081237694fb18da51bf4d976a856210bd
jonpena <gran_200014@hotmail.com>
2025-01-28 06:00:10 +0100

    fix: :bug: removing peer in icon-button

--- client/src/components/UI/icon-button.tsx
+++ client/src/components/UI/icon-button.tsx
@@ -11,7 +11,7 @@ export const IconButton = ({ icon, className, ...props }: IconButtonProps) => {
   return (
     <button
       className={cn(
-        'group peer w-8 h-8 flex flex-shrink-0 items-center justify-center rounded-lg bg-black/5 dark:bg-neutral-800 transition-all hover:bg-black/10 disabled:opacity-50 disabled:pointer-events-none touch-none cursor-default z-0 aspect-square',
+        'group w-8 h-8 flex flex-shrink-0 items-center justify-center rounded-lg bg-black/5 dark:bg-neutral-800 transition-all hover:bg-black/10 disabled:opacity-50 disabled:pointer-events-none touch-none cursor-default z-0 aspect-square',
         className
       )}
       {...props}
