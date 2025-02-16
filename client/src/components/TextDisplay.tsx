474ee465c7d3d8eb869e9907d885e2f5ed4a4c7e
jonpena <gran_200014@hotmail.com>
2025-02-16 03:33:52 +0100

    docs: :art: rename component

---
+++ client/src/components/TextDisplay.tsx
@@ -0,0 +1,41 @@
+import { Strikethrough } from '@/components/ui/strikethrough';
+import { cn } from '@/lib/utils';
+
+type TextDisplayProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  taskName: string;
+  date: string;
+  checked: boolean;
+  classNameButton?: string;
+};
+
+export const TextDisplay = ({
+  taskName,
+  checked,
+  date,
+  classNameButton,
+  className,
+  ...props
+}: TextDisplayProps) => {
+  return (
+    <button
+      onMouseDown={(e) => e.preventDefault()}
+      className={cn(
+        `cursor-text absolute pt-3.5 left-0 z-0 w-full h-full rounded-md flex items-start text-left 
+    peer-focus:pointer-events-none peer-focus:opacity-0 peer-focus:!text-transparent`,
+        classNameButton
+      )}
+      {...props}
+    >
+      <span
+        className={cn(
+          `pl-9 ml-8 truncate text-sm ${
+            date && !checked ? 'w-[calc(100%-9.5rem)]' : 'w-[calc(100%-6.5rem)]'
+          }`,
+          className
+        )}
+      >
+        <Strikethrough checked={checked}>{taskName}</Strikethrough>
+      </span>
+    </button>
+  );
+};
