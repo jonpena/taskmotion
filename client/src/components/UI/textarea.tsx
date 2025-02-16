3ea98d91f08fbc67ed641b8e7dbd855a79e1810d
jonpena <gran_200014@hotmail.com>
2025-02-16 03:40:46 +0100

    style: :lipstick: ajust light mode UI

--- client/src/components/UI/textarea.tsx
+++ client/src/components/UI/textarea.tsx
@@ -14,6 +14,7 @@ export const Textarea = ({ className, ...rest }: TextareaProps) => {
         `w-full pl-3 pr-12 py-1 text-sm rounded-md
           bg-neutral-200 dark:bg-neutral-900 
           placeholder:text-muted-foreground
+          text-muted-foreground
           focus:outline-none focus:ring-0 focus:ring-transparent
           resize-none transition-all duration-200
            border-none focus-visible:ring-0 outline-none text-pretty`,
