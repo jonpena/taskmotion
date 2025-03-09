b3ff7c9c8ead688b80564a92eb0b1c6a69eeda79
jonpena <gran_200014@hotmail.com>
2025-03-09 14:46:39 +0100

    feat: implement notifications route and service for managing user notifications

---
+++ server/src/routes/notifications.ts
@@ -0,0 +1,50 @@
+import { supabaseMiddleware } from '@server/middleware/supabase';
+import { createNotification, getNotifications, updateNotifications } from '@server/services/notifications';
+import { INotification } from '@shared/notification.interface';
+import { Hono } from 'hono';
+
+export const notificationsApp = new Hono();
+
+notificationsApp.use('*', supabaseMiddleware);
+
+notificationsApp.get('/:email', async (c) => {
+
+  const { data, error } = await getNotifications(c, c.req.param('email') as string);  
+
+  if (error) return c.json({ error }, 400);
+
+  if (data && data.length > 0) {
+    return c.json({ data: data[0].notifications }, 200);
+  }
+
+  return c.json({data}, 200);
+});
+
+notificationsApp.put('/:email', async (c) => {
+  const email = c.req.param('email');
+  const body = (await c.req.json()) as INotification;
+
+  // First check if notifications exist for this email
+  const { data: existingData, error: fetchError } = await getNotifications(c, email);
+  
+  if (fetchError) return c.json({ error: fetchError }, 400);
+  
+  let result;
+  
+  // If notifications exist, append the new one
+  if (existingData && existingData.length > 0) {
+    // Assuming notifications is an array in the existing record
+    const existingNotifications = existingData[0].notifications || [];
+    const updatedNotifications = [body,...existingNotifications];
+    // Update the existing notifications
+    result = await updateNotifications(c, email, updatedNotifications);
+  } else {
+    // If no notifications exist, create a new record
+    result = await createNotification(c, email, body);
+  }
+
+  const { data, error } = result;
+  if (error) return c.json({ error }, 400);
+
+  return c.json({ data }, 200);
+});
