5d2de7b682f50f7caf6945f9c7c05fa708058bc9
jonpena <gran_200014@hotmail.com>
2025-03-18 14:27:54 +0100

    fix: remove SWRConfig wrapper from main.tsx to simplify component structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,20 +7,14 @@ import { ThemeProvider } from '@/context/ThemeContext.tsx';
 import App from './App.tsx';
 
 import { StrictMode } from 'react';
-import { SWRConfig, SWRConfiguration } from 'swr';
-import { localStorageProvider } from './services/localStorageProvider.ts';
-
-const swrConfig = { provider: localStorageProvider } as SWRConfiguration;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <SWRConfig value={swrConfig}>
-          <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
-            <App />
-          </ThemeProvider>
-        </SWRConfig>
+        <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+          <App />
+        </ThemeProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>
