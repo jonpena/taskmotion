f578d712281c45ab2c88964fb534389777235089
jonpena <gran_200014@hotmail.com>
2025-03-28 17:55:48 +0100

    fix: enhance generateDescription function to accept and improve existing descriptions

--- server/src/routes/ai.ts
+++ server/src/routes/ai.ts
@@ -4,21 +4,18 @@ import { generateDescription } from '@server/services/ai';
 export const aiApp = new Hono();
 
 aiApp.get('/generateDescription', async (c: Context) => {
-  if (!c.env.API_KEY) {
-    return c.text('API_KEY not found in wrangler.toml', 500);
-  }
+  if (!c.env.API_KEY) return c.text('API_KEY not found in wrangler.toml', 500);
 
   const taskName = c.req.query('task');
+  const description = c.req.query('description');
 
-  if (!taskName) {
+  if (!taskName)
     return c.json(
       {
-        error: 'El parámetro "task" es requerido',
+        error: 'Task name is required',
       },
       400
     );
-  }
-  const task = c.req.query('task') as string;
-  const result = await generateDescription(c, task);
+  const result = await generateDescription(c, taskName, description);
   return c.json({ result });
 });
--- server/src/services/ai.ts
+++ server/src/services/ai.ts
@@ -1,20 +1,28 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { Context } from "hono";
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { Context } from 'hono';
 
-export const generateDescription = async (c: Context, task: string) => {
+export const generateDescription = async (
+  c: Context,
+  task: string,
+  description: string = ''
+) => {
   const genAI = new GoogleGenerativeAI(c.env.API_KEY);
 
   const generationConfig = {
-    temperature: 0.2, maxOutputTokens: 200
-  }
+    temperature: 0.2,
+    maxOutputTokens: 200,
+  };
 
   const model = genAI.getGenerativeModel({
     model: 'gemini-2.0-flash',
     generationConfig,
   });
 
+  if (description) {
+    return await model.generateContent(`
+    Toma la siguiente descripción y mejórala haciéndola más clara, concisa y profesional. Usa un lenguaje natural sin símbolos, listas ni enumeraciones. Incorpora emojis de manera sutil para hacerla más visual y atractiva sin que se vea forzada. La descripción es: ${description}.`);
+  }
+
   return await model.generateContent(`
-    crear descripcion de "${task}". no enumerar, crear 
-    parrafos, conciso y que tenga un maximo de 600 caracteres`
-  );
-}
+    Dada la siguiente tarea, genera una descripción clara y concisa explicando qué se debe hacer. Usa un tono profesional y directo sin listas, símbolos ni enumeraciones. Incorpora emojis de manera natural para hacerla más visual y atractiva. La tarea es: ${task}.`);
+};
