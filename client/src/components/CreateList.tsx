511ce886d98467b87111955f7b72770b8ef17020
jonpena <gran_200014@hotmail.com>
2025-05-15 19:21:16 +0200

    refactor: replace Input and related components with CreateInput for task and list creation

--- client/src/components/CreateList.tsx
+++ client/src/components/CreateList.tsx
@@ -3,23 +3,19 @@ import { requestCreateList } from '@/services/requestCreateList';
 import { useListStore } from '@/store/listStore';
 import { useEffect, useRef, useState } from 'react';
 import { nanoid } from 'nanoid';
-import { Input } from './ui/input';
 import { replaceEmojis } from '@/utils/replaceEmojis';
 import { useNavigate } from 'react-router-dom';
 import { useShortcut } from '@/hooks/useShortcut';
-import { useMediaQuery } from '@uidotdev/usehooks';
-import { AddButton } from './buttons/AddButton';
-import { ShortcutBadge } from './buttons/ShortcutBadge';
 import { SIZE_ID } from '@/constants/base';
 import { createNotification } from '@/utils/createNotification';
 import { useNotificationsStore } from '@/store/notificationsStore';
+import { CreateInput } from './CreateInput';
 
 const CreateList = () => {
   const [listName, setListName] = useState('');
   const inputRef = useRef(null!) as React.RefObject<HTMLInputElement>;
   const { email } = UserAuth().user;
   const keydown = useShortcut(['ctrl+l']);
-  const isSmallDevice = useMediaQuery('only screen and (max-width : 1023px)');
   const navigate = useNavigate();
   const { lists, setLists } = useListStore();
   const notificationsStore = useNotificationsStore();
@@ -38,7 +34,6 @@ const CreateList = () => {
       navigate(`/b/${newlist.listId}`);
       inputRef.current?.blur();
 
-      // Create notification
       createNotification(notificationsStore, email, {
         type: 'list',
         action: 'created',
@@ -47,50 +42,24 @@ const CreateList = () => {
     }
   };
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const handleListCreation = () => {
     listName ? createList() : inputRef.current?.focus();
   };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      listName ? createList() : inputRef.current?.focus();
-    }
-  };
-
-  const handleKeyPress = (e: React.KeyboardEvent) =>
-    e.key === 'Enter' && createList();
 
   useEffect(() => {
     if (keydown === 'ctrl+l') inputRef.current?.focus();
   }, [keydown]);
 
   return (
-    <div
-      className='mt-1 w-full sticky bottom-0 flex items-center group bg-neutral-100 
-    dark:bg-neutral-900 rounded-md px-2'
-    >
-      <Input
-        ref={inputRef}
-        type='text'
-        value={listName}
-        onChange={(e) => setListName(e.target.value.trimStart())}
-        onKeyDown={handleKeyPress}
-        placeholder='Create new list...'
-        className='text-neutral-600 dark:text-neutral-50 px-2 outline-none w-full h-12 border-none focus-visible:ring-0 focus-visible:placeholder:text-neutral-400 dark:focus-visible:placeholder:text-neutral-100 rounded-none shadow-none'
-      />
-
-      {!isSmallDevice && (
-        <ShortcutBadge keys='L' className={`${listName && 'opacity-0'}`} />
-      )}
-
-      <AddButton
-        title='Create new list'
-        onMouseDown={handleClick}
-        onKeyDown={handleKeyDown}
-      />
-    </div>
+    <CreateInput
+      value={listName}
+      onChange={setListName}
+      onSubmit={handleListCreation}
+      inputRef={inputRef}
+      placeholder='Create new list...'
+      shortcutKey='l'
+      className='mt-1'
+    />
   );
 };
 
--- client/src/components/CreateTask.tsx
+++ client/src/components/CreateTask.tsx
@@ -4,19 +4,15 @@ import { useTaskStore } from '../store/taskStore';
 import { useParams } from 'react-router-dom';
 import { requestUpdateList } from '@/services/requestUpdateList';
 import { useListStore } from '@/store/listStore';
-import { Input } from '@/components/ui/input';
-import { Checkbox } from '@/components/ui/checkbox';
 import { replaceEmojis } from '@/utils/replaceEmojis';
-import { MAX_CONTENT_TASK, SIZE_ID } from '@/constants/base';
+import { SIZE_ID } from '@/constants/base';
 import { useShortcut } from '@/hooks/useShortcut';
-import { useMediaQuery } from '@uidotdev/usehooks';
 import { format } from 'date-fns';
 import { CalendarButton } from './buttons/CalendarButton';
-import { ShortcutBadge } from './buttons/ShortcutBadge';
-import { AddButton } from './buttons/AddButton';
 import { UserAuth } from '@/context/AuthContext';
 import { createNotification } from '@/utils/createNotification';
 import { useNotificationsStore } from '@/store/notificationsStore';
+import { CreateInput } from './CreateInput';
 
 const CreateTask = () => {
   const [taskName, setTaskName] = useState('');
@@ -26,7 +22,6 @@ const CreateTask = () => {
   const [checked, setChecked] = useState(false);
   const inputRef = useRef(null!) as React.RefObject<HTMLInputElement>;
   const keydown = useShortcut(['ctrl+e']);
-  const isSmallDevice = useMediaQuery('only screen and (max-width : 1023px)');
   const [date, setDate] = useState<string | undefined>(undefined);
   const { email } = UserAuth().user;
   const notificationsStore = useNotificationsStore();
@@ -52,28 +47,11 @@ const CreateTask = () => {
       setTaskName('');
       setLists([...updateLists]);
 
-      // Create notification
       createNotification(notificationsStore, email, {
         type: 'task',
         action: 'created',
         message: taskName,
       });
-    }
-  };
-
-  const handleKeyPress = (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter') createTask();
-  };
-
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    handleTaskCreation();
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      handleTaskCreation();
     }
   };
 
@@ -86,44 +64,55 @@ const CreateTask = () => {
   }, [keydown]);
 
   return (
-    <div className='relative group flex items-center w-full bg-gray-100 dark:bg-neutral-900 rounded-md px-2'>
-      <Checkbox
-        name='checked'
-        checked={checked}
-        className='transition-all duration-1000'
-        onChange={(e) => setChecked(e.target.checked)}
-        onKeyDown={(e) => e.key === 'Enter' && setChecked(!checked)}
-      />
-
-      <Input
-        type='text'
-        maxLength={MAX_CONTENT_TASK}
-        ref={inputRef}
-        placeholder='Create new task...'
-        className='text-neutral-600 dark:text-neutral-50 px-3 outline-none w-full h-12 border-none focus-visible:ring-0 focus-visible:placeholder:text-neutral-400 dark:focus-visible:placeholder:text-neutral-100 rounded-none shadow-none'
-        onKeyDown={handleKeyPress}
-        onChange={(e) => setTaskName(e.target.value.trimStart())}
-        value={taskName}
-      />
-
-      {!isSmallDevice && (
-        <ShortcutBadge keys='E' className={`${taskName && 'opacity-0'}`} />
-      )}
-
+    <CreateInput
+      checked={checked}
+      onCheck={setChecked}
+      value={taskName}
+      onChange={setTaskName}
+      onSubmit={handleTaskCreation}
+      inputRef={inputRef}
+      placeholder='Create new task...'
+      shortcutKey='e'
+    >
       <CalendarButton
         date={date}
         setDate={setDate}
-        className={`opacity-0 group-focus-within:opacity-100 ${
-          !taskName && 'opacity-0'
-        }`}
+        // className={`opacity-0 group-focus-within:opacity-100 ${
+        //   !taskName && 'opacity-0'
+        // }`}
       />
+    </CreateInput>
 
-      <AddButton
-        title='Create new task'
-        onMouseDown={handleClick}
-        onKeyDown={handleKeyDown}
-      />
-    </div>
+    // <div className='relative group flex items-center w-full bg-gray-100 dark:bg-neutral-900 rounded-md px-2'>
+    //    <Checkbox
+    //     name='checked'
+    //     checked={checked}
+    //     className='transition-all duration-1000'
+    //     onChange={(e) => setChecked(e.target.checked)}
+    //     onKeyDown={(e) => e.key === 'Enter' && setChecked(!checked)}
+    //   />
+
+    //   <Input
+    //     type='text'
+    //     maxLength={MAX_CONTENT_TASK}
+    //     ref={inputRef}
+    //     placeholder='Create new task...'
+    //     className='text-neutral-600 dark:text-neutral-50 px-3 outline-none w-full h-12 border-none focus-visible:ring-0 focus-visible:placeholder:text-neutral-400 dark:focus-visible:placeholder:text-neutral-100 rounded-none shadow-none'
+    //     onKeyDown={handleKeyPress}
+    //     onChange={(e) => setTaskName(e.target.value.trimStart())}
+    //     value={taskName}
+    //   />
+
+    //   {!isSmallDevice && (
+    //     <ShortcutBadge keys='E' className={`${taskName && 'opacity-0'}`} />
+    //   )} */}
+
+    //   <AddButton
+    //     title='Create new task'
+    //     onMouseDown={handleClick}
+    //     onKeyDown={handleKeyDown}
+    //   />
+    // </div>
   );
 };
 
