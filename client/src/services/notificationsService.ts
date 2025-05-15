2a9e283a0959b0b2cf01fc8549a281b54c49f187
jonpena <gran_200014@hotmail.com>
2025-05-15 23:32:43 +0200

    feat: add notifications service with get and update functionality

---
+++ client/src/services/notificationsService.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { getApiBaseUrl } from '@/utils/getApiBaseUrl';
+import { INotification } from '@shared/notification.interface';
+
+export const getNotifications = async (
+  email: string
+): Promise<INotification[]> => {
+  try {
+    const { data } = await axios.get(
+      `${getApiBaseUrl()}api/notifications/${email}`
+    );
+    return data.data;
+  } catch (error: unknown) {
+    throw new Error(`Error al obtener las notificaciones`);
+  }
+};
+
+export const updateNotifications = async (
+  email: string,
+  body: INotification
+): Promise<INotification[]> => {
+  try {
+    const { data } = await axios.put(
+      `${getApiBaseUrl()}api/notifications/${email}`,
+      body
+    );
+    return data.data;
+  } catch (error: unknown) {
+    throw new Error(`Error al actualizar las notificaciones`);
+  }
+};
