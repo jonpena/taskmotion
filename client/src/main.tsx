adc90a2fb215edf7112f4cdeab814999fcef86de
jonpena <gran_200014@hotmail.com>
2025-07-10 03:30:13 +0200

    refactor: code structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,7 +7,7 @@ import { AuthContextProvider } from '@/context/AuthContext.tsx';
 import { ThemeProvider } from '@/context/ThemeContext.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
